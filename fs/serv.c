6caf630802859689c79308dd4b8c9ba94362087b
leo <hireedleo@gmail.com>
2021-11-23 00:35:15 -0800

    finished lab5's Ex5 and passed 'serve_open/file_stat/file_close' and 'file_read' for a score of 70/150

--- fs/serv.c
+++ fs/serv.c
@@ -209,12 +209,20 @@ serve_read(envid_t envid, union Fsipc *ipc)
 {
 	struct Fsreq_read *req = &ipc->read;
 	struct Fsret_read *ret = &ipc->readRet;
+	struct OpenFile *o;
+	int r;
 
 	if (debug)
 		cprintf("serve_read %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// Lab 5: Your code here:
-	return 0;
+	if ((r = openfile_lookup(envid, req->req_fileid, &o)) < 0)
+		return r;
+	
+	r = file_read(o->o_file, ret->ret_buf, req->req_n, o->o_fd->fd_offset);
+	if (r == 0)
+		o->o_fd->fd_offset += r;	// update seek position.
+	return r;
 }
 
 
