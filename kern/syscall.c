30eccb71aedb3b00f3729929f93d381f76841e22
leo <hireedleo@gmail.com>
2021-11-23 03:08:53 -0800

    finished lab5's Ex7 and added a new syscall "sys_env_set_trapframe"

--- kern/syscall.c
+++ kern/syscall.c
@@ -142,7 +142,26 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	struct Env* p_env = NULL;
+	int r;
+
+	if ((r = envid2env(envid, &p_env, 1)) < 0)
+		return r;
+	
+	// make sure user env run at CPL==3
+	tf->tf_es = GD_UD | 3;
+	tf->tf_ds = GD_UD | 3;
+	tf->tf_ss = GD_UD | 3;
+	tf->tf_cs = GD_UT | 3;
+
+	// enable interrupts
+	tf->tf_eflags |= FL_IF;
+
+	// set IOPL = 0
+	tf->tf_eflags &= ~(FL_IOPL_3);
+
+	memmove(&p_env->env_tf, tf, sizeof(*tf));
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -449,6 +468,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		break;
 	case SYS_ipc_recv:
 		retval = sys_ipc_recv((void*)a1);
+		break;
+	case SYS_env_set_trapframe:
+		retval = sys_env_set_trapframe(a1, (void*)a2);
 		break;
 	default:
 		return -E_INVAL;
