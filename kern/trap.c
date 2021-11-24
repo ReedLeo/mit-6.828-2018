10ae454441292dba24b2c5f9a46a8b164194db47
leo <hireedleo@gmail.com>
2021-11-23 18:41:27 -0800

    finished lab5's Ex9, passed test case of 'testkbd'

--- kern/trap.c
+++ kern/trap.c
@@ -214,14 +214,26 @@ trap_dispatch(struct Trapframe *tf)
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
 
-	// Handle keyboard and serial interrupts.
-	// LAB 5: Your code here.
-
 	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
 		lapic_eoi();
 		sched_yield();
 		return;
 	}
+
+	
+	// Handle keyboard and serial interrupts.
+	// LAB 5: Your code here.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_KBD) {
+		kbd_intr();
+		return;
+	}
+
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SERIAL) {
+		serial_intr();
+		return;
+	}
+
+
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
