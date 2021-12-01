4174317468843aea4902b8768def5fa697aa4c43
leo <hireedleo@gmail.com>
2021-12-01 06:16:42 -0800

    finished Lab6's Ex5. added initialization fucntion of e1000

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,14 +1,57 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
-volatile
+#define TX_MAX_NDESC 64         // number of tx descriptors
+#define TX_MAX_BUF_SIZE 1520    // 1518 round up to 16-algined, in bytes
+
+// TCTL bit values
+#define TCTL_EN         (1 << 1)
+#define TCTL_PSP        (1 << 3)
+#define TCTL_CT(x)      (((uint8_t)x) << 4)
+#define TCTL_COLD(x)    ((x & 0x3ff) << 12)
+#define TCTL_COLD_HALF  TCTL_COLD(512)
+#define TCTL_COLD_FULL  TCTL_COLD(64)
+
+static volatile
 uint32_t *e1000_base_addr;
 
+static
+struct e1000_tx_desc tx_descs[TX_MAX_NDESC];
+
+static
+uint8_t tx_bufs[TX_MAX_NDESC][TX_MAX_BUF_SIZE];
+
 // LAB 6: Your driver code here
+static
+void pci_e1000_init() 
+{
+    for (uint32_t i = 0; i < TX_MAX_NDESC; ++i) {
+        tx_descs[i].buffer_addr = PADDR(tx_bufs[i]);
+    }
+
+    // set TDBA that consists of TDBAH:TDBAL these 2 32-bit registers.
+    e1000_base_addr[E1000_TDBAL >> 2] = PADDR(tx_descs);
+    e1000_base_addr[E1000_TDBAH >> 2] = 0;
+
+    // set TDLEN to the size(in bytes) of the descriptor ring.
+    e1000_base_addr[E1000_TDLEN >> 2] = sizeof(tx_descs);
+
+    // set TDH/TDT to 0
+    e1000_base_addr[E1000_TDH >> 2] = e1000_base_addr[E1000_TDT >> 2] = 0;
+
+    // set TCTL (control register)
+    e1000_base_addr[E1000_TXDCTL >> 2] = TCTL_EN | TCTL_PSP | TCTL_CT(0x10) | TCTL_COLD_FULL;
+
+    // set TIPG
+    e1000_base_addr[E1000_TIPG >> 2] = E1000_DEFAULT_TIPG_IPGT
+                                     | (E1000_DEFAULT_TIPG_IPGR1 << E1000_TIPG_IPGR1_SHIFT)
+                                     | (E1000_DEFAULT_TIPG_IPGR2 << E1000_TIPG_IPGR2_SHIFT);
+}
+
 int pci_e1000_attach(struct pci_func* pcif) 
 {
     pci_func_enable(pcif);
     e1000_base_addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    cprintf("in pci_e1000_attach: e1000's BAR 0 = %x, and its status register contains %x\n", e1000_base_addr, e1000_base_addr[2]);
+    pci_e1000_init();
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,6 +2,58 @@
 #define JOS_KERN_E1000_H
 #include <kern/pci.h>
 
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TXDCTL   0x03828  /* TX Descriptor Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */
+
+/* Default values for the transmit IPG register */
+#define E1000_DEFAULT_TIPG_IPGT     10
+#define E1000_DEFAULT_TIPG_IPGR1    4
+#define E1000_DEFAULT_TIPG_IPGR2    6
+#define E1000_TIPG_IPGT_MASK        0x000003FF
+#define E1000_TIPG_IPGR1_MASK       0x000FFC00
+#define E1000_TIPG_IPGR2_MASK       0x3FF00000
+#define E1000_TIPG_IPGR1_SHIFT      10
+#define E1000_TIPG_IPGR2_SHIFT      20
+
+/* Transmit Descriptor */
+struct e1000_tx_desc {
+    uint64_t buffer_addr;       /* Address of the descriptor's data buffer */
+    union {
+        uint32_t data;
+        struct {
+            uint16_t length;    /* Data buffer length */
+            uint8_t cso;        /* Checksum offset */
+            uint8_t cmd;        /* Descriptor control */
+        } flags;
+    } lower;
+    union {
+        uint32_t data;
+        struct {
+            uint8_t status;     /* Descriptor status */
+            uint8_t css;        /* Checksum start */
+            uint16_t special;
+        } fields;
+    } upper;
+};
+
 int pci_e1000_attach(struct pci_func* pcif);
 
 #endif  // SOL >= 6
