48f2d9e1306e09b79e1a5dcc653b9fad454cff49
leo <hireedleo@gmail.com>
2021-11-30 17:48:40 -0800

    finished Lab6's Ex4. created a virtual mapping for the E1000's BAR 0

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,9 +1,14 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+volatile
+uint32_t *e1000_base_addr;
+
 // LAB 6: Your driver code here
 int pci_e1000_attach(struct pci_func* pcif) 
 {
     pci_func_enable(pcif);
+    e1000_base_addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("in pci_e1000_attach: e1000's BAR 0 = %x, and its status register contains %x\n", e1000_base_addr, e1000_base_addr[2]);
     return 0;
 }
