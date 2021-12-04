a76acefda1bfcc899dbb8e04921495c2fedbb2e0
leo <hireedleo@gmail.com>
2021-12-05 00:54:47 +0800

    finished Lab6's Ex10. set up the receive descriptors and queue buffers.

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,8 +2,11 @@
 #include <kern/pmap.h>
 #include <inc/string.h>
 
-#define TX_MAX_NDESC 64         // number of tx descriptors
-#define TX_MAX_BUF_SIZE 1520    // 1518 round up to 16-algined, in bytes
+#define TX_MAX_NDESC    64          // number of tx descriptors
+#define TX_MAX_BUF_SIZE 1520        // 1518 round up to 16-algined, in bytes
+
+#define RX_MAX_NDESC    128         // number of rd descriptors
+#define RX_MAX_BUF_SIZE 2048        // 
 
 // TCTL bit values
 #define TCTL_EN         (1 << 1)
@@ -18,6 +21,8 @@
 
 #define TXD_STAT_DD     1
 
+#define E1000_REG(offset) (e1000_base_addr[(offset)>>2])
+
 static volatile
 uint32_t *e1000_base_addr;
 
@@ -26,6 +31,12 @@ struct e1000_tx_desc tx_descs[TX_MAX_NDESC];
 
 static
 uint8_t tx_bufs[TX_MAX_NDESC][TX_MAX_BUF_SIZE];
+
+static
+struct e1000_rx_desc rx_desc[RX_MAX_NDESC];
+
+static
+uint8_t rx_bufs[RX_MAX_NDESC][RX_MAX_BUF_SIZE];
 
 // LAB 6: Your driver code here
 static
@@ -38,20 +49,20 @@ void transmit_init()
     }
 
     // set TDBA that consists of TDBAH:TDBAL these 2 32-bit registers.
-    e1000_base_addr[E1000_TDBAL >> 2] = PADDR(tx_descs);
-    e1000_base_addr[E1000_TDBAH >> 2] = 0;
+    E1000_REG(E1000_TDBAL) = PADDR(tx_descs);
+    E1000_REG(E1000_TDBAH) = 0;
 
     // set TDLEN to the size(in bytes) of the descriptor ring.
-    e1000_base_addr[E1000_TDLEN >> 2] = sizeof(tx_descs);
+    E1000_REG(E1000_TDLEN) = sizeof(tx_descs);
 
     // set TDH/TDT to 0
-    e1000_base_addr[E1000_TDH >> 2] = e1000_base_addr[E1000_TDT >> 2] = 0;
+    E1000_REG(E1000_TDH) = E1000_REG(E1000_TDT) = 0;
 
     // set TCTL (control register)
-    e1000_base_addr[E1000_TCTL >> 2] = TCTL_EN | TCTL_PSP | TCTL_CT(0x10) | TCTL_COLD_FULL;
+    E1000_REG(E1000_TCTL) = TCTL_EN | TCTL_PSP | TCTL_CT(0x10) | TCTL_COLD_FULL;
 
     // set TIPG
-    e1000_base_addr[E1000_TIPG >> 2] = E1000_DEFAULT_TIPG_IPGT
+    E1000_REG(E1000_TIPG) = E1000_DEFAULT_TIPG_IPGT
                                      | (E1000_DEFAULT_TIPG_IPGR1 << E1000_TIPG_IPGR1_SHIFT)
                                      | (E1000_DEFAULT_TIPG_IPGR2 << E1000_TIPG_IPGR2_SHIFT);
 }
@@ -59,7 +70,34 @@ void transmit_init()
 static
 void receive_init()
 {
+    memset(rx_desc, 0, sizeof(rx_desc));
+    for (uint32_t i = 0; i < RX_MAX_NDESC; ++i) {
+        rx_desc[i].buffer_addr = PADDR(rx_bufs[i]); 
+    }
 
+    // set RAL/RAH to QEMU's MAC address 52:54:00:12:34:56.
+    E1000_REG(E1000_RA) = 0x12005452;   // RAL0
+    E1000_REG(E1000_RA + 4) = 0x5634 | E1000_RAH_AV;   // RAH0 and validate RAL0:RAH0
+
+    // set MTA, Multicast Table Array
+    E1000_REG(E1000_MTA) = 0;
+
+    // set RDBAL/RDBAH
+    E1000_REG(E1000_RDBAL) = PADDR(rx_desc);
+    E1000_REG(E1000_RDBAH) = 0;
+
+    // set RDLEN
+    E1000_REG(E1000_RDLEN) = sizeof(rx_desc);
+
+    // set RDH/RDT
+    E1000_REG(E1000_RDH) = 0;
+    E1000_REG(E1000_RDT) = RX_MAX_NDESC;
+
+    // set RCTL
+    E1000_REG(E1000_RCTL) = E1000_RCTL_EN 
+                          | E1000_RCTL_SBP
+                          | E1000_RCTL_SECRC
+                          | E1000_RCTL_SZ_2048;
 }
 
 static
@@ -71,7 +109,7 @@ void pci_e1000_init()
 
 int e1000_transmit(const char* buf, int size)
 {
-    uint32_t tdt_idx = e1000_base_addr[E1000_TDT >> 2];
+    uint32_t tdt_idx = E1000_REG(E1000_TDT);
     struct e1000_tx_desc* p_next_desc = tx_descs + tdt_idx;
     
     // pakcet is too big to transmit.
@@ -96,7 +134,7 @@ int e1000_transmit(const char* buf, int size)
 
     // update TDT
     tdt_idx = (tdt_idx + 1) % TX_MAX_NDESC;
-    e1000_base_addr[E1000_TDT >> 2]  = tdt_idx;
+    E1000_REG(E1000_TDT)  = tdt_idx;
 
     return size;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,6 +3,12 @@
 #include <kern/pci.h>
 
 /* Registers */
+#define E1000_RCTL     0x00100  /* RX Control - RW */
+#define E1000_RDBAL    0x02800  /* RX Descriptor Base Address Low - RW */
+#define E1000_RDBAH    0x02804  /* RX Descriptor Base Address High - RW */
+#define E1000_RDLEN    0x02808  /* RX Descriptor Length - RW */
+#define E1000_RDH      0x02810  /* RX Descriptor Head - RW */
+#define E1000_RDT      0x02818  /* RX Descriptor Tail - RW */
 #define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
 #define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
 #define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
@@ -10,6 +16,11 @@
 #define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
 #define E1000_TCTL     0x00400  /* TX Control - RW */
 #define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_MTA      0x05200  /* Multicast Table Array - RW Array */
+#define E1000_RA       0x05400  /* Receive Address - RW Array */
+
+/* Receive Address */
+#define E1000_RAH_AV  0x80000000        /* Receive descriptor valid */
 
 /* Transmit Control */
 #define E1000_TCTL_RST    0x00000001    /* software reset */
@@ -75,6 +86,83 @@ struct e1000_tx_desc {
             uint16_t special;
         } fields;
     } upper;
+};
+
+/* Receive Descriptor bit definitions */
+#define E1000_RXD_STAT_DD       0x01    /* Descriptor Done */
+#define E1000_RXD_STAT_EOP      0x02    /* End of Packet */
+#define E1000_RXD_STAT_IXSM     0x04    /* Ignore checksum */
+#define E1000_RXD_STAT_VP       0x08    /* IEEE VLAN Packet */
+#define E1000_RXD_STAT_UDPCS    0x10    /* UDP xsum caculated */
+#define E1000_RXD_STAT_TCPCS    0x20    /* TCP xsum calculated */
+#define E1000_RXD_STAT_IPCS     0x40    /* IP xsum calculated */
+#define E1000_RXD_STAT_PIF      0x80    /* passed in-exact filter */
+#define E1000_RXD_STAT_IPIDV    0x200   /* IP identification valid */
+#define E1000_RXD_STAT_UDPV     0x400   /* Valid UDP checksum */
+#define E1000_RXD_STAT_ACK      0x8000  /* ACK Packet indication */
+#define E1000_RXD_ERR_CE        0x01    /* CRC Error */
+#define E1000_RXD_ERR_SE        0x02    /* Symbol Error */
+#define E1000_RXD_ERR_SEQ       0x04    /* Sequence Error */
+#define E1000_RXD_ERR_CXE       0x10    /* Carrier Extension Error */
+#define E1000_RXD_ERR_TCPE      0x20    /* TCP/UDP Checksum Error */
+#define E1000_RXD_ERR_IPE       0x40    /* IP Checksum Error */
+#define E1000_RXD_ERR_RXE       0x80    /* Rx Data Error */
+#define E1000_RXD_SPC_VLAN_MASK 0x0FFF  /* VLAN ID is in lower 12 bits */
+#define E1000_RXD_SPC_PRI_MASK  0xE000  /* Priority is in upper 3 bits */
+#define E1000_RXD_SPC_PRI_SHIFT 13
+#define E1000_RXD_SPC_CFI_MASK  0x1000  /* CFI is bit 12 */
+#define E1000_RXD_SPC_CFI_SHIFT 12
+
+/* Receive Control */
+#define E1000_RCTL_RST            0x00000001    /* Software reset */
+#define E1000_RCTL_EN             0x00000002    /* enable */
+#define E1000_RCTL_SBP            0x00000004    /* store bad packet */
+#define E1000_RCTL_UPE            0x00000008    /* unicast promiscuous enable */
+#define E1000_RCTL_MPE            0x00000010    /* multicast promiscuous enab */
+#define E1000_RCTL_LPE            0x00000020    /* long packet enable */
+#define E1000_RCTL_LBM_NO         0x00000000    /* no loopback mode */
+#define E1000_RCTL_LBM_MAC        0x00000040    /* MAC loopback mode */
+#define E1000_RCTL_LBM_SLP        0x00000080    /* serial link loopback mode */
+#define E1000_RCTL_LBM_TCVR       0x000000C0    /* tcvr loopback mode */
+#define E1000_RCTL_DTYP_MASK      0x00000C00    /* Descriptor type mask */
+#define E1000_RCTL_DTYP_PS        0x00000400    /* Packet Split descriptor */
+#define E1000_RCTL_RDMTS_HALF     0x00000000    /* rx desc min threshold size */
+#define E1000_RCTL_RDMTS_QUAT     0x00000100    /* rx desc min threshold size */
+#define E1000_RCTL_RDMTS_EIGTH    0x00000200    /* rx desc min threshold size */
+#define E1000_RCTL_MO_SHIFT       12            /* multicast offset shift */
+#define E1000_RCTL_MO_0           0x00000000    /* multicast offset 11:0 */
+#define E1000_RCTL_MO_1           0x00001000    /* multicast offset 12:1 */
+#define E1000_RCTL_MO_2           0x00002000    /* multicast offset 13:2 */
+#define E1000_RCTL_MO_3           0x00003000    /* multicast offset 15:4 */
+#define E1000_RCTL_MDR            0x00004000    /* multicast desc ring 0 */
+#define E1000_RCTL_BAM            0x00008000    /* broadcast enable */
+/* these buffer sizes are valid if E1000_RCTL_BSEX is 0 */
+#define E1000_RCTL_SZ_2048        0x00000000    /* rx buffer size 2048 */
+#define E1000_RCTL_SZ_1024        0x00010000    /* rx buffer size 1024 */
+#define E1000_RCTL_SZ_512         0x00020000    /* rx buffer size 512 */
+#define E1000_RCTL_SZ_256         0x00030000    /* rx buffer size 256 */
+/* these buffer sizes are valid if E1000_RCTL_BSEX is 1 */
+#define E1000_RCTL_SZ_16384       0x00010000    /* rx buffer size 16384 */
+#define E1000_RCTL_SZ_8192        0x00020000    /* rx buffer size 8192 */
+#define E1000_RCTL_SZ_4096        0x00030000    /* rx buffer size 4096 */
+#define E1000_RCTL_VFE            0x00040000    /* vlan filter enable */
+#define E1000_RCTL_CFIEN          0x00080000    /* canonical form enable */
+#define E1000_RCTL_CFI            0x00100000    /* canonical form indicator */
+#define E1000_RCTL_DPF            0x00400000    /* discard pause frames */
+#define E1000_RCTL_PMCF           0x00800000    /* pass MAC control frames */
+#define E1000_RCTL_BSEX           0x02000000    /* Buffer size extension */
+#define E1000_RCTL_SECRC          0x04000000    /* Strip Ethernet CRC */
+#define E1000_RCTL_FLXBUF_MASK    0x78000000    /* Flexible buffer size */
+#define E1000_RCTL_FLXBUF_SHIFT   27            /* Flexible buffer shift */
+
+/* Receive Descriptor */
+struct e1000_rx_desc {
+    uint64_t buffer_addr; /* Address of the descriptor's data buffer */
+    uint16_t length;     /* Length of data DMAed into data buffer */
+    uint16_t csum;       /* Packet checksum */
+    uint8_t status;      /* Descriptor status */
+    uint8_t errors;      /* Descriptor Errors */
+    uint16_t special;
 };
 
 #define E_TX_ERROR  0x10000000          /* transmit error code base. */
